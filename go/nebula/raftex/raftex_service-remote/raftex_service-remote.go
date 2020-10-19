479970dea0d86291fa6e5076a74cdb8f8798bf43
Aiee <18348405+Aiee@users.noreply.github.com>
2020-10-19 10:52:11 +0800

    update thrift files

--- go/nebula/raftex/raftex_service-remote/raftex_service-remote.go
+++ go/nebula/raftex/raftex_service-remote/raftex_service-remote.go
@@ -14,7 +14,7 @@ import (
         "strconv"
         "strings"
         thrift "github.com/facebook/fbthrift/thrift/lib/go/thrift"
-        "../../github.com/vesoft-inc/nebula-go/v2/nebula/raftex"
+        "../../github.com/vesoft-inc/nebula-clients/go/nebula/raftex"
 )
 
 func Usage() {
