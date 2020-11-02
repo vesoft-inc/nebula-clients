a6bd7f01a45890b1f9d317a46dca58a5e05962b5
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-11-02 15:56:12 +0800

    modify thrift directory

---
+++ python/nebula2/__init__.py
@@ -0,0 +1,15 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+
+__all__ = ['common', 'graph', 'meta', 'storage', 'data', 'net', 'Config', 'Exception']
+
+import sys
+import os
+import nebula2
+nebula_path = os.path.dirname(nebula2.__file__)
+sys.path.insert(0, nebula_path)
+
