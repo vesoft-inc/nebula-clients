002483c680b3d6ee801a0ecbdab5eea04f4d560d
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-11-02 16:49:54 +0800

    modify head

--- python/nebula2/__init__.py
+++ python/nebula2/__init__.py
@@ -1,8 +1,10 @@
-/* Copyright (c) 2020 vesoft inc. All rights reserved.
- *
- * This source code is licensed under Apache 2.0 License,
- * attached with Common Clause Condition 1.0, found in the LICENSES directory.
- */
+#!/usr/bin/env python
+# --coding:utf-8--
+
+# Copyright (c) 2020 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
 
 
 __all__ = ['common', 'graph', 'meta', 'storage', 'data', 'net', 'Config', 'Exception']
@@ -12,4 +14,3 @@ import os
 import nebula2
 nebula_path = os.path.dirname(nebula2.__file__)
 sys.path.insert(0, nebula_path)
-
