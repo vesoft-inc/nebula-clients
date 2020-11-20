d14084282f908779848f2db32956a592a125b2cb
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2020-11-20 16:17:03 +0800

    correct  storage test port

--- java/client/src/test/java/com/vesoft/nebula/client/graph/storage/StorageClientTest.java
+++ java/client/src/test/java/com/vesoft/nebula/client/graph/storage/StorageClientTest.java
@@ -6,21 +6,12 @@
 
 package com.vesoft.nebula.client.graph.storage;
 
-import com.google.common.collect.Maps;
 import com.google.common.net.HostAndPort;
 import com.vesoft.nebula.client.graph.NebulaPoolConfig;
-import com.vesoft.nebula.client.graph.storage.data.Edge;
-import com.vesoft.nebula.client.graph.storage.data.Vertex;
-import com.vesoft.nebula.client.graph.storage.scan.ScanEdgeResult;
-import com.vesoft.nebula.client.graph.storage.scan.ScanEdgeResultIterator;
-import com.vesoft.nebula.client.graph.storage.scan.ScanVertexResult;
-import com.vesoft.nebula.client.graph.storage.scan.ScanVertexResultIterator;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import junit.framework.TestCase;
 
-public class StorageClientTest extends TestCase {
+public class StorageClientTest {
 
     private StorageConnPool pool;
     private final String spaceName = "test1";
@@ -31,7 +22,6 @@ public class StorageClientTest extends TestCase {
      * init pool
      */
     public void setUp() throws Exception {
-        MockNebulaGraph.createGraph();
         List<HostAndPort> address = Arrays.asList(
                 HostAndPort.fromParts(ip, 45500),
                 HostAndPort.fromParts(ip, 45501),
@@ -52,159 +42,7 @@ public class StorageClientTest extends TestCase {
             client = pool.getStorageClient(addr);
         } catch (Exception e) {
             e.printStackTrace();
-            fail();
         }
         return client;
-    }
-
-    /**
-     * test scan vertex with noColumns
-     */
-    public void testScanVertexWithNoColumn() {
-        StorageClient client = getClient();
-        // test scan specific cols
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("person", Arrays.asList("name"));
-        ScanVertexResultIterator iterator;
-        try {
-            iterator = client.scanVertex(spaceName, returnCols, true);
-            ScanVertexResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Vertex vertex : result.getAllVertices()) {
-                    System.out.println(vertex.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    /**
-     * test scan vertex with all columns
-     */
-    public void testScanVertexWithAllColumns() {
-        StorageClient client = getClient();
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("person", Arrays.asList());
-        ScanVertexResultIterator iterator;
-        try {
-            iterator = client.scanVertex(spaceName, returnCols, false);
-            ScanVertexResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Vertex vertex : result.getAllVertices()) {
-                    System.out.println(vertex.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-
-    /**
-     * test scan vertex with all columns
-     */
-    public void testScanVertexWithSpecificColumns() {
-        StorageClient client = getClient();
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("person", Arrays.asList("name"));
-        ScanVertexResultIterator iterator;
-        try {
-            iterator = client.scanVertex(spaceName, returnCols, false);
-            ScanVertexResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Vertex vertex : result.getAllVertices()) {
-                    System.out.println(vertex.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    /**
-     * test scan edge with no columns
-     */
-    public void testScanEdgeWithNoColumns() {
-        StorageClient client = getClient();
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("friend", Arrays.asList("duration"));
-        ScanEdgeResultIterator iterator;
-        try {
-            iterator = client.scanEdge(spaceName, returnCols, true);
-            ScanEdgeResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Edge edge : result.getAllEdges()) {
-                    assert (edge.getEdgeType().getProps().size() == 0);
-                    System.out.println(edge.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    /**
-     * test scan edge with all columns
-     */
-    public void testScanEdgeWithAllColumns() {
-        StorageClient client = getClient();
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("friend", Arrays.asList());
-        ScanEdgeResultIterator iterator;
-        try {
-            iterator = client.scanEdge(spaceName, returnCols, false);
-            ScanEdgeResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Edge edge : result.getAllEdges()) {
-                    assert (edge.getEdgeType().getProps().size() == 3);
-                    System.out.println(edge.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    /**
-     * test scan edge with specific columns
-     */
-    public void testScanEdgeWithSpecificColumns() {
-        StorageClient client = getClient();
-        Map<String, List<String>> returnCols = Maps.newHashMap();
-        returnCols.put("friend", Arrays.asList("duration", "relation"));
-        ScanEdgeResultIterator iterator;
-        try {
-            iterator = client.scanEdge(spaceName, returnCols, false);
-            ScanEdgeResult result;
-            while (iterator.hasNext()) {
-                result = iterator.next();
-                for (Edge edge : result.getAllEdges()) {
-                    assert (edge.getEdgeType().getProps().size() == 2);
-                    assert (edge.getEdgeType().getRank() == 0);
-                    System.out.println(edge.toString());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-
-    public void testClose() {
-        pool.close();
-        assert (pool.getNumActive(HostAndPort.fromParts(ip, 45500)) == 0);
-
     }
 }
