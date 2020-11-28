588b57fa951b35477cba7576bb64d978a1c76cee
laura-ding <48548375+laura-ding@users.noreply.github.com>
2020-11-28 14:51:19 +0800

    Add DataSetWrapper (#36)
    * add data set wrapper
    
    * add data set wrapper

--- python/nebula2/gclient/data/ResultSet.py
+++ python/nebula2/gclient/data/ResultSet.py
@@ -7,61 +7,8 @@
 # attached with Common Clause Condition 1.0, found in the LICENSES directory.
 
 from nebula2.graph import ttypes
-from nebula2.Exception import (
-    OutOfRangeException,
-    InvalidKeyException
-)
 
-from nebula2.gclient.data.DataObject import ValueWrapper
-
-
-class Record(object):
-    def __init__(self, values, names):
-        assert len(names) == len(values)
-        self._record = list()
-        self._names = names
-
-        for val in values:
-            self._record.append(ValueWrapper(val))
-
-    def __iter__(self):
-        return iter(self._record)
-
-    def size(self):
-        return len(self._names)
-
-    def get_value(self, index):
-        '''
-        get value by index
-        :return: Value
-        '''
-        if index >= len(self._names):
-            raise OutOfRangeException()
-        return self._record[index]
-
-    def get_value(self, key):
-        '''
-        get value by key
-        :return: Value
-        '''
-        if key not in self._names:
-            raise InvalidKeyException(key)
-        return self._record[self._names[key]]
-
-    def values(self):
-        return self._record
-
-    def __repr__(self):
-        return "{}({})".format(self.__class__.__name__, self._record)
-
-    def __eq__(self, other):
-        if not isinstance(other, self.__class__):
-            return False
-
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not (self == other)
+from nebula2.gclient.data.DataObject import DataSetWrapper
 
 
 class ResultSet(object):
@@ -71,14 +18,9 @@ class ResultSet(object):
         '''
         self._decode_type = decode_type
         self._resp = resp
-        self._pos = -1
-        self._key_indexes = {}
-        self._column_names = []
+        self._data_set_wrapper = None
         if self._resp.data is not None:
-            for index, name in enumerate(self._resp.data.column_names):
-                d_name = name.decode(self._decode_type)
-                self._column_names.append(d_name)
-                self._key_indexes[d_name] = index
+            self._data_set_wrapper = DataSetWrapper(resp.data, self._decode_type)
 
     def is_succeeded(self):
         return self._resp.error_code == ttypes.ErrorCode.SUCCEEDED
@@ -87,16 +29,18 @@ class ResultSet(object):
         return self._resp.error_code
 
     def space_name(self):
-        if self._resp.space_name is None:
-            return ''
+        if self._resp.error_msg is None:
+            return None
         return self._resp.space_name.decode(self._decode_type)
 
     def error_msg(self):
         if self._resp.error_msg is None:
-            return ''
+            return None
         return self._resp.error_msg.decode(self._decode_type)
 
     def comment(self):
+        if self._resp.error_msg is None:
+            return None
         return self._resp.comment.decode(self._decode_type)
 
     def latency(self):
@@ -109,31 +53,31 @@ class ResultSet(object):
         return self._resp.plan_desc
 
     def is_empty(self):
-        return self._resp.data is None or len(self._resp.data.rows) == 0
+        return self._data_set_wrapper is None or len(self._data_set_wrapper.rows) == 0
 
     def keys(self):
         '''
         get colNames
         '''
-        if self._resp.data is None:
-            raise RuntimeError('Data is none')
-        return self._column_names
+        if self._data_set_wrapper is None:
+            return []
+        return self._data_set_wrapper.get_col_names()
 
     def row_size(self):
         '''
         get one row size
         '''
-        if self._resp.data is None:
-            raise RuntimeError('Data is none')
-        return len(self._resp.data.rows)
+        if self._data_set_wrapper is None:
+            return 0
+        return len(self._data_set_wrapper.get_rows())
 
     def col_size(self):
         '''
         get one col size
         '''
-        if self._resp.data is None:
-            raise RuntimeError('Data is none')
-        return len(self._column_names)
+        if self._data_set_wrapper is None:
+            return 0
+        return len(self._data_set_wrapper.get_col_names())
 
     def get_row_types(self):
         '''
@@ -157,9 +101,9 @@ class ResultSet(object):
           ttypes.Value.UVAL = 14
           ttypes.Value.GVAL = 15
         '''
-        if self._resp.data is None:
+        if self._data_set_wrapper is None:
             return []
-        return [(value.getType()) for value in self._resp.data.rows[0].values]
+        return self._data_set_wrapper.get_row_types()
 
     def row_values(self, row_index):
         '''
@@ -167,11 +111,9 @@ class ResultSet(object):
         :param index: the Record index
         :return: list<ValueWrapper>
         '''
-        if self._resp.data is None:
-            raise RuntimeError('Data is none')
-        if row_index >= len(self._resp.data.rows):
-            raise OutOfRangeException()
-        return [(ValueWrapper(value)) for value in self._resp.data.rows[row_index].values]
+        if self._data_set_wrapper is None:
+            return []
+        return self._data_set_wrapper.row_values(row_index)
 
     def column_values(self, key):
         '''
@@ -179,12 +121,9 @@ class ResultSet(object):
         :param key: the col name
         :return: list<ValueWrapper>
         '''
-        if self._resp.data is None:
-            raise RuntimeError('Data is none')
-        if key not in self._column_names:
-            raise InvalidKeyException(key)
-
-        return [(ValueWrapper(row.values[self._key_indexes[key]])) for row in self._resp.data.rows]
+        if self._data_set_wrapper is None:
+            return []
+        return self._data_set_wrapper.column_values(key)
 
     def rows(self):
         '''
@@ -192,22 +131,12 @@ class ResultSet(object):
         :param key: empty
         :return: list<Row>
         '''
-        if self._resp.data is None:
-            return None
-        return self._resp.data.rows
+        if self._data_set_wrapper is None:
+            return []
+        return self._data_set_wrapper.get_rows()
 
     def __iter__(self):
-        return self
-
-    def __next__(self):
-        '''
-        The record iterator
-        :return: recode
-        '''
-        if len(self._resp.data.rows) == 0 or self._pos >= len(self._resp.data.rows) - 1:
-            raise StopIteration
-        self._pos = self._pos + 1
-        return Record(self._resp.data.rows[self._pos].values, self._column_names)
+        return iter(self._data_set_wrapper)
 
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, self._resp)
